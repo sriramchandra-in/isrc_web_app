355b1a087bf967f685b497b6ad7b3cffe4d405cd
rvakkada <rvakkada@bloomberg.met>
2024-12-23 13:37:32 -0500

    converting to html

--- app.py
+++ app.py
@@ -7,17 +7,26 @@ from xml_utils import create_babuji_message_map, create_kcv_message_map
 
 load_dotenv()
 
+def convert_text_to_html(txt: str):
+    src = f'{txt}.txt'
+    dest = f'{txt}.html'
+    contents = open(src, 'r')
+    with open(dest, 'w') as f:
+        for line in contents.readlines():
+            f.write(f'<pre>{line}</pre>')
+
 def main():
-    config = dotenv_values(".env")
-    config_dir = config.get("CONFIG_DIR")
-    babuji_xml = config.get("BABUJI_XML")
-    kcv_xml = config.get("KCV_XML")
-    babuji_abs_xml = config_dir + "/" + babuji_xml
-    print(babuji_abs_xml)
-    kcv_abs_xml = config_dir + "/" + kcv_xml
-    print(kcv_abs_xml)
-    babuji_message_map = create_babuji_message_map(babuji_abs_xml)
-    kcv_message_map = create_kcv_message_map(kcv_abs_xml)
+    # config = dotenv_values(".env")
+    # config_dir = config.get("CONFIG_DIR")
+    # babuji_xml = config.get("BABUJI_XML")
+    # kcv_xml = config.get("KCV_XML")
+    # babuji_abs_xml = config_dir + "/" + babuji_xml
+    # print(babuji_abs_xml)
+    # kcv_abs_xml = config_dir + "/" + kcv_xml
+    # print(kcv_abs_xml)
+    #babuji_message_map = create_babuji_message_map(babuji_abs_xml)
+    #kcv_message_map = create_kcv_message_map(kcv_abs_xml)
+    convert_text_to_html("config/txt/divine_light")
 
 if __name__ == '__main__':
   main()
