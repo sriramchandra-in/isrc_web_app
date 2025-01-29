25da60c44f8f382a299dddc8921ec91295e23b6d
Rajkumar Vakkada <rajkumar.vakkada@gmail.com>
2025-01-29 07:48:45 -0500

    02-02-25

---
+++ centenary.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="Sriramchandra.org" name="author" />
+        <meta content="sriramchandra.org, raja yoga, pranahuti, purpose of meditation,institute of sri ramchandra conciousness"
+         name="keywords" />
+         <meta content="Institure of sri ramchandra conciousness" name="description" />
+
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/selectize.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/vanillabox/vanillabox.css" type="text/css">
+
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <style>
+.collapsible {
+        background-color: #DCE0EF;
+        color: #006;
+        cursor: pointer;
+        padding: 18px;
+        width: 100%;
+        border: none;
+        text-align: left;
+        outline: none;
+        font-size: 15px;
+        font-weight: 600;
+}
+.middle (
+Margin: left;
+}
+
+
+.content {
+    padding: 0 18px;
+    display: none;
+    overflow: hidden;
+    background-color: #f1f1f1;
+
+}
+.newcontainer1{
+    width: 90%;
+    padding:30px;
+    Margin:left;
+}
+
+.table_title{
+
+        width:100%;
+        border:1px;
+        Margin:left;
+}
+span {
+  display: table-cell;
+  vertical-align: bottom;
+  padding:10px;
+}
+
+</style>
+
+     <title>Institute of Sri Ramchandra Consciousness --- Daily Message </title>
+</head>
+<body class="page-sub-page page-about-us">
+<!-- Wrapper -->
+<div class="wrapper">
+<!-- Header -->
+
+
+<?php
+include_once('includes/header.php');
+
+?>
+
+<!-- Breadcrumb -->
+
+    <ol class="breadcrumb">
+        <li><a href="index.php">Home</a></li>
+        <li class="active"> daily Messages</li>
+    </ol>
+</div>
+<!-- end Breadcrumb -->
+
+<!-- Page Content -->
+<div id="page-content">
+    <div class="container">
+    <div class="row">
+            <!--MAIN Content-->
+            <div class="col-md-8">
+                <div id="page-main">
+                  <section id="Prayer">
+
+
+<div class="newcontainer1">
+
+
+
+
+<div class ="top-left">
+
+    <h3><b>Pujya Babuji Reading Centenary Message !</b></h3>
+    <p style="font-size:22px; color:#C00; ">
+
+    </p>
+        <video  width="320" height="240" controls>
+            <source src="videos/lalaji_centenary.mp4#t=363" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+</div>
+
+
+<?php
+include_once('includes/footer.php');
+
+?>
