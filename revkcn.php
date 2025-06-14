8010fae4c4b5fe5a59c0f5128d3853159d31320b
vakkadar <rajkumar.vakkada@gmail.com>
2025-06-14 10:02:32 -0400

    adding recent changes done outside git for june 11 and june 13

---
+++ revkcn.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="Sriramchandra.org" name="author" />
+        <meta content="sriramchandra.org, raja yoga, pranahuti, purpose of meditation,institute of sri ramchandra conciousness"
+         name="keywords" />
+         <meta content="Institure of sri ramchandra conciousness" name="description" />
+
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/selectize.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/vanillabox/vanillabox.css" type="text/css">
+
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <style>
+.collapsible {
+        background-color: #DCE0EF;
+        color: #006;
+        cursor: pointer;
+        padding: 18px;
+        width: 100%;
+        border: none;
+        text-align: left;
+        outline: none;
+        font-size: 15px;
+        font-weight: 600;
+}
+.middle (
+Margin: left;
+}
+
+
+.content {
+    padding: 0 18px;
+    display: none;
+    overflow: hidden;
+    background-color: #f1f1f1;
+
+}
+.newcontainer1{
+    width: 90%;
+    padding:30px;
+    Margin:left;
+}
+
+.table_title{
+
+        width:100%;
+        border:1px;
+        Margin:left;
+}
+span {
+  display: table-cell;
+  vertical-align: bottom;
+  padding:10px;
+}
+
+</style>
+
+     <title>Institute of Sri Ramchandra Consciousness --- Daily Message </title>
+</head>
+<body class="page-sub-page page-about-us">
+<!-- Wrapper -->
+<div class="wrapper">
+<!-- Header -->
+
+
+<?php
+include_once('includes/header.php');
+
+?>
+
+<!-- Breadcrumb -->
+
+    <ol class="breadcrumb">
+        <li><a href="index.php">Home</a></li>
+        <li class="active"> daily Messages</li>
+    </ol>
+</div>
+<!-- end Breadcrumb -->
+
+<!-- Page Content -->
+
+
+
+
+
+
+<div style="width:800px; margin:0 auto;" >
+
+    <h3 align="center" style="color:#0000ff "><b>Remembering our Guide Pujya K.C.Narayana garu's Birthday with gratitude!</b></h3>
+    </br>
+    <p style="font-size:22px; color:#C00; ">
+On the auspicious day of the birthday of our Guide Pujya Sri K.C.Narayana garu, we express our gratitude by dedicating ourselves to work to fulfill the mission of our Master Pujya Babuji Maharaj. In order to live such a life pregnant with the high morals, Pujya K.C.Narayana garu emphasised the importance of the naroow path of self effort and individual responsibility for spiritual development to make ourselves deserving vessels to travel in the Path of Grace (through Pranahuti).</p><br/> <p style="font-size:22px; color:#C00; ">Though the literature of ISRC is replete with the guidance of Puya K.C.Narayana garu, one of the articles on attitudes to proper sadhana is linked below.</p> 
+    </p>
+<h3 align="center" style="color:#0000ff "> <a href="https://sriramchandra.in/books/BP1/mobile/index.html#p=95">Attitudes for Proper Sadhana</a></h3>
+<p style="font-size:22px; color:#C00; ">An article delineating the etching of the sketch in the Path of Grace of Spirituality of Pujya K.C.Narayana garu which is  an inspiring glimpse into the spiritual sadhana and journey in the Path of the Great Master is also linked below.</p>
+<h3 align="center" style="color:#0000ff "><a href="https://kcnarayana.org/About/Spirituality.htm">Sketch of Pujya Sir's Spiritual Journey</a></h3>	
+</div>
+
+<?php
+include_once('includes/footer.php');
+
+?>
